1c7cb05ee09cf6547f972ba480ebdf9b9cbedc7e
MarcoPC <marcomatute@unah.hn>
2019-01-11 04:19:48 -0600

    Modificacion nav bar

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,5 +1,5 @@
 
-// ------------Create charts
+// ------------Create charts-----------
 //Chart Pantalones
 var chart = Highcharts.chart('chart', {
     chart: {
@@ -9,16 +9,15 @@ var chart = Highcharts.chart('chart', {
       text: 'Sales by month for: '
     },
     subtitle: {
-      text: 'Sales by Month'
+      text: 'Sales Report'
     },
     xAxis: {
-      type: 'category'
+      type: 'Category'
     },
     yAxis: {
       title: {
-        text: 'Ventas'
+        text: 'Sales'
       }
-  
     },
     legend: {
       enabled: false
@@ -73,19 +72,19 @@ var chart = Highcharts.chart('chart', {
           "id": "January",
           "data": [
             [
-              "v48.0",
+              "Semana 1",
               0.13
             ],
             [
-              "v47.0",
+              "Semana 2",
               0.11
             ],
             [
-              "v43.0",
+              "Semana 3",
               0.17
             ],
             [
-              "v29.0",
+              "Semana 4",
               0.26
             ]
           ]
@@ -96,19 +95,19 @@ var chart = Highcharts.chart('chart', {
           "data": [
             
             [
-              "v54.0",
+              "Semana 1",
               0.1
             ],
             [
-              "v51.0",
+              "Semana 2",
               0.15
             ],
             [
-              "v50.0",
+              "Semana 3",
               0.1
             ],
             [
-              "v48.0",
+              "Semana 4",
               0.31
             ]
           ]
@@ -118,19 +117,19 @@ var chart = Highcharts.chart('chart', {
           "id": "March",
           "data": [
             [
-              "v11.0",
+              "Semana 1",
               6.2
             ],
             [
-              "v10.0",
+              "Semana 2",
               0.29
             ],
             [
-              "v9.0",
+              "Semana 3",
               0.27
             ],
             [
-              "v8.0",
+              "Semana 4",
               0.47
             ]
           ]
@@ -140,19 +139,19 @@ var chart = Highcharts.chart('chart', {
           "id": "April",
           "data": [
             [
-              "v10.0",
+              "Semana 1",
               0.36
             ],
             [
-              "v9.1",
+              "Semana 2",
               0.54
             ],
             [
-              "v9.0",
+              "Semana 3",
               0.13
             ],
             [
-              "v5.1",
+              "Semana 4",
               0.2
             ]
           ]
@@ -400,587 +399,15 @@ var chart2 = Highcharts.chart('chart2', {
     }
   });
 
-//Chart Pantalones Skinnys
-var chart3 = Highcharts.chart('chart', {
-    chart: {
-      type: 'column'
-    },
-    title: {
-      text: 'Sales by month for: '
-    },
-    subtitle: {
-      text: 'Sales by Month'
-    },
-    xAxis: {
-      type: 'category'
-    },
-    yAxis: {
-      title: {
-        text: 'Ventas'
-      }
-  
-    },
-    legend: {
-      enabled: false
-    },
-    plotOptions: {
-      series: {
-        borderWidth: 0,
-        dataLabels: {
-          enabled: true,
-          format: '{point.y:.1f}'
-        }
-      }
-    },
-  
-    tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b> total of sales.<br/>'
-    },
-  
-    "series": [
-      {
-        "name": "Months",
-        "colorByPoint": true,
-        "data": [
-          {
-            "name": "January",
-            "y": 450,
-            "drilldown": "January"
-          },
-          {
-            "name": "February",
-            "y": 200,
-            "drilldown": "February"
-          },
-          {
-            "name": "March",
-            "y": 100,
-            "drilldown": "March"
-          },
-          {
-            "name": "April",
-            "y": 250,
-            "drilldown": "April"
-          }
-        ]
-      }
-    ],
-    "drilldown": {
-      "series": [
-        {
-          "name": "January",
-          "id": "January",
-          "data": [
-            [
-              "v65.0",
-              0.1
-            ],
-            [
-              "v64.0",
-              1.3
-            ],
-            [
-              "v63.0",
-              53.02
-            ],
-            [
-              "v62.0",
-              1.4
-            ],
-            [
-              "v61.0",
-              0.88
-            ],
-            [
-              "v60.0",
-              0.56
-            ],
-            [
-              "v59.0",
-              0.45
-            ],
-            [
-              "v58.0",
-              0.49
-            ],
-            [
-              "v57.0",
-              0.32
-            ],
-            [
-              "v56.0",
-              0.29
-            ],
-            [
-              "v55.0",
-              0.79
-            ],
-            [
-              "v54.0",
-              0.18
-            ],
-            [
-              "v51.0",
-              0.13
-            ],
-            [
-              "v49.0",
-              2.16
-            ],
-            [
-              "v48.0",
-              0.13
-            ],
-            [
-              "v47.0",
-              0.11
-            ],
-            [
-              "v43.0",
-              0.17
-            ],
-            [
-              "v29.0",
-              0.26
-            ]
-          ]
-        },
-        {
-          "name": "February",
-          "id": "February",
-          "data": [
-            [
-              "v58.0",
-              1.02
-            ],
-            [
-              "v57.0",
-              7.36
-            ],
-            [
-              "v56.0",
-              0.35
-            ],
-            [
-              "v55.0",
-              0.11
-            ],
-            [
-              "v54.0",
-              0.1
-            ],
-            [
-              "v52.0",
-              0.95
-            ],
-            [
-              "v51.0",
-              0.15
-            ],
-            [
-              "v50.0",
-              0.1
-            ],
-            [
-              "v48.0",
-              0.31
-            ],
-            [
-              "v47.0",
-              0.12
-            ]
-          ]
-        },
-        {
-          "name": "March",
-          "id": "March",
-          "data": [
-            [
-              "v11.0",
-              6.2
-            ],
-            [
-              "v10.0",
-              0.29
-            ],
-            [
-              "v9.0",
-              0.27
-            ],
-            [
-              "v8.0",
-              0.47
-            ]
-          ]
-        },
-        {
-          "name": "April",
-          "id": "April",
-          "data": [
-            [
-              "v11.0",
-              3.39
-            ],
-            [
-              "v10.1",
-              0.96
-            ],
-            [
-              "v10.0",
-              0.36
-            ],
-            [
-              "v9.1",
-              0.54
-            ],
-            [
-              "v9.0",
-              0.13
-            ],
-            [
-              "v5.1",
-              0.2
-            ]
-          ]
-        },
-        {
-          "name": "Edge",
-          "id": "Edge",
-          "data": [
-            [
-              "v16",
-              2.6
-            ],
-            [
-              "v15",
-              0.92
-            ],
-            [
-              "v14",
-              0.4
-            ],
-            [
-              "v13",
-              0.1
-            ]
-          ]
-        },
-        {
-          "name": "Opera",
-          "id": "Opera",
-          "data": [
-            [
-              "v50.0",
-              0.96
-            ],
-            [
-              "v49.0",
-              0.82
-            ],
-            [
-              "v12.1",
-              0.14
-            ]
-          ]
-        }
-      ]
-    }
-});
-//Chart Pantalones Slimns
-var chart4 = Highcharts.chart('chart2', {
-    chart: {
-      type: 'column'
-    },
-    title: {
-      text: 'Sales by month for: '
-    },
-    subtitle: {
-      text: 'Sales by Month'
-    },
-    xAxis: {
-      type: 'category'
-    },
-    yAxis: {
-      title: {
-        text: 'Ventas'
-      }
-  
-    },
-    legend: {
-      enabled: false
-    },
-    plotOptions: {
-      series: {
-        borderWidth: 0,
-        dataLabels: {
-          enabled: true,
-          format: '{point.y:.1f}'
-        }
-      }
-    },
-  
-    tooltip: {
-      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b> total of sales.<br/>'
-    },
-  
-    "series": [
-      {
-        "name": "Months",
-        "colorByPoint": true,
-        "data": [
-          {
-            "name": "January",
-            "y": 600,
-            "drilldown": "January"
-          },
-          {
-            "name": "February",
-            "y": 400,
-            "drilldown": "February"
-          },
-          {
-            "name": "March",
-            "y": 250,
-            "drilldown": "March"
-          },
-          {
-            "name": "April",
-            "y": 400,
-            "drilldown": "April"
-          }
-        ]
-      }
-    ],
-    "drilldown": {
-      "series": [
-        {
-          "name": "January",
-          "id": "January",
-          "data": [
-            [
-              "v65.0",
-              0.1
-            ],
-            [
-              "v64.0",
-              1.3
-            ],
-            [
-              "v63.0",
-              53.02
-            ],
-            [
-              "v62.0",
-              1.4
-            ],
-            [
-              "v61.0",
-              0.88
-            ],
-            [
-              "v60.0",
-              0.56
-            ],
-            [
-              "v59.0",
-              0.45
-            ],
-            [
-              "v58.0",
-              0.49
-            ],
-            [
-              "v57.0",
-              0.32
-            ],
-            [
-              "v56.0",
-              0.29
-            ],
-            [
-              "v55.0",
-              0.79
-            ],
-            [
-              "v54.0",
-              0.18
-            ],
-            [
-              "v51.0",
-              0.13
-            ],
-            [
-              "v49.0",
-              2.16
-            ],
-            [
-              "v48.0",
-              0.13
-            ],
-            [
-              "v47.0",
-              0.11
-            ],
-            [
-              "v43.0",
-              0.17
-            ],
-            [
-              "v29.0",
-              0.26
-            ]
-          ]
-        },
-        {
-          "name": "February",
-          "id": "February",
-          "data": [
-            [
-              "v58.0",
-              1.02
-            ],
-            [
-              "v57.0",
-              7.36
-            ],
-            [
-              "v56.0",
-              0.35
-            ],
-            [
-              "v55.0",
-              0.11
-            ],
-            [
-              "v54.0",
-              0.1
-            ],
-            [
-              "v52.0",
-              0.95
-            ],
-            [
-              "v51.0",
-              0.15
-            ],
-            [
-              "v50.0",
-              0.1
-            ],
-            [
-              "v48.0",
-              0.31
-            ],
-            [
-              "v47.0",
-              0.12
-            ]
-          ]
-        },
-        {
-          "name": "March",
-          "id": "March",
-          "data": [
-            [
-              "v11.0",
-              6.2
-            ],
-            [
-              "v10.0",
-              0.29
-            ],
-            [
-              "v9.0",
-              0.27
-            ],
-            [
-              "v8.0",
-              0.47
-            ]
-          ]
-        },
-        {
-          "name": "April",
-          "id": "April",
-          "data": [
-            [
-              "v11.0",
-              3.39
-            ],
-            [
-              "v10.1",
-              0.96
-            ],
-            [
-              "v10.0",
-              0.36
-            ],
-            [
-              "v9.1",
-              0.54
-            ],
-            [
-              "v9.0",
-              0.13
-            ],
-            [
-              "v5.1",
-              0.2
-            ]
-          ]
-        },
-        {
-          "name": "Edge",
-          "id": "Edge",
-          "data": [
-            [
-              "v16",
-              2.6
-            ],
-            [
-              "v15",
-              0.92
-            ],
-            [
-              "v14",
-              0.4
-            ],
-            [
-              "v13",
-              0.1
-            ]
-          ]
-        },
-        {
-          "name": "Opera",
-          "id": "Opera",
-          "data": [
-            [
-              "v50.0",
-              0.96
-            ],
-            [
-              "v49.0",
-              0.82
-            ],
-            [
-              "v12.1",
-              0.14
-            ]
-          ]
-        }
-      ]
-    }
-});
+
 
 // Modify type of chart
   $('#plain').click(function () {
     chart.update({
       chart: {
         inverted: false,
-        polar: false
+        polar: false,
+        type: 'bar'
       },
       subtitle: {
         text: 'Plain'
@@ -1005,8 +432,9 @@ var chart4 = Highcharts.chart('chart2', {
   $('#inverted').click(function () {
     chart.update({
       chart: {
-        inverted: true,
-        polar: false
+        inverted: false,
+        polar: false,
+        type: 'bar'
       },
       subtitle: {
         text: 'Inverted'
@@ -1030,7 +458,8 @@ var chart4 = Highcharts.chart('chart2', {
     chart.update({
       chart: {
         inverted: false,
-        polar: true
+        polar: true,
+        type: column
       },
       subtitle: {
         text: 'Polar'
@@ -1047,4 +476,33 @@ var chart4 = Highcharts.chart('chart2', {
           text: 'Polar'
         }
       });
+  });
+
+  $('#pie').click(function () {
+    chart.update({
+  
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+      },
+      subtitle: {
+        text: 'Pie'
+      }
+    });
+  });
+  $('#pie2').click(function () {
+    chart.update({
+  
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+      },
+      subtitle: {
+        text: 'Pie'
+      }
+    });
   });
