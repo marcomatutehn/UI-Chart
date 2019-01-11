66b165839c74b0c2cb438727273b0ed5b4154982
MarcoPC <marcomatute@unah.hn>
2019-01-11 00:53:38 -0600

    Implementacion de bootstrap

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,5 +1,6 @@
 
-// Create charts
+// ------------Create charts
+//Chart Pantalones
 var chart = Highcharts.chart('chart', {
     chart: {
       type: 'column'
@@ -286,7 +287,7 @@ var chart = Highcharts.chart('chart', {
       ]
     }
   });
-
+//Chart Camisas
 var chart2 = Highcharts.chart('chart2', {
     chart: {
       type: 'column'
@@ -574,6 +575,581 @@ var chart2 = Highcharts.chart('chart2', {
     }
   });
 
+//Chart Pantalones Skinnys
+var chart3 = Highcharts.chart('chart', {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Sales by month for: '
+    },
+    subtitle: {
+      text: 'Sales by Month'
+    },
+    xAxis: {
+      type: 'category'
+    },
+    yAxis: {
+      title: {
+        text: 'Ventas'
+      }
+  
+    },
+    legend: {
+      enabled: false
+    },
+    plotOptions: {
+      series: {
+        borderWidth: 0,
+        dataLabels: {
+          enabled: true,
+          format: '{point.y:.1f}'
+        }
+      }
+    },
+  
+    tooltip: {
+      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b> total of sales.<br/>'
+    },
+  
+    "series": [
+      {
+        "name": "Months",
+        "colorByPoint": true,
+        "data": [
+          {
+            "name": "January",
+            "y": 450,
+            "drilldown": "Chrome"
+          },
+          {
+            "name": "February",
+            "y": 200,
+            "drilldown": "Firefox"
+          },
+          {
+            "name": "March",
+            "y": 100,
+            "drilldown": "Internet Explorer"
+          },
+          {
+            "name": "April",
+            "y": 250,
+            "drilldown": "Safari"
+          }
+        ]
+      }
+    ],
+    "drilldown": {
+      "series": [
+        {
+          "name": "Chrome",
+          "id": "Chrome",
+          "data": [
+            [
+              "v65.0",
+              0.1
+            ],
+            [
+              "v64.0",
+              1.3
+            ],
+            [
+              "v63.0",
+              53.02
+            ],
+            [
+              "v62.0",
+              1.4
+            ],
+            [
+              "v61.0",
+              0.88
+            ],
+            [
+              "v60.0",
+              0.56
+            ],
+            [
+              "v59.0",
+              0.45
+            ],
+            [
+              "v58.0",
+              0.49
+            ],
+            [
+              "v57.0",
+              0.32
+            ],
+            [
+              "v56.0",
+              0.29
+            ],
+            [
+              "v55.0",
+              0.79
+            ],
+            [
+              "v54.0",
+              0.18
+            ],
+            [
+              "v51.0",
+              0.13
+            ],
+            [
+              "v49.0",
+              2.16
+            ],
+            [
+              "v48.0",
+              0.13
+            ],
+            [
+              "v47.0",
+              0.11
+            ],
+            [
+              "v43.0",
+              0.17
+            ],
+            [
+              "v29.0",
+              0.26
+            ]
+          ]
+        },
+        {
+          "name": "Firefox",
+          "id": "Firefox",
+          "data": [
+            [
+              "v58.0",
+              1.02
+            ],
+            [
+              "v57.0",
+              7.36
+            ],
+            [
+              "v56.0",
+              0.35
+            ],
+            [
+              "v55.0",
+              0.11
+            ],
+            [
+              "v54.0",
+              0.1
+            ],
+            [
+              "v52.0",
+              0.95
+            ],
+            [
+              "v51.0",
+              0.15
+            ],
+            [
+              "v50.0",
+              0.1
+            ],
+            [
+              "v48.0",
+              0.31
+            ],
+            [
+              "v47.0",
+              0.12
+            ]
+          ]
+        },
+        {
+          "name": "Internet Explorer",
+          "id": "Internet Explorer",
+          "data": [
+            [
+              "v11.0",
+              6.2
+            ],
+            [
+              "v10.0",
+              0.29
+            ],
+            [
+              "v9.0",
+              0.27
+            ],
+            [
+              "v8.0",
+              0.47
+            ]
+          ]
+        },
+        {
+          "name": "Safari",
+          "id": "Safari",
+          "data": [
+            [
+              "v11.0",
+              3.39
+            ],
+            [
+              "v10.1",
+              0.96
+            ],
+            [
+              "v10.0",
+              0.36
+            ],
+            [
+              "v9.1",
+              0.54
+            ],
+            [
+              "v9.0",
+              0.13
+            ],
+            [
+              "v5.1",
+              0.2
+            ]
+          ]
+        },
+        {
+          "name": "Edge",
+          "id": "Edge",
+          "data": [
+            [
+              "v16",
+              2.6
+            ],
+            [
+              "v15",
+              0.92
+            ],
+            [
+              "v14",
+              0.4
+            ],
+            [
+              "v13",
+              0.1
+            ]
+          ]
+        },
+        {
+          "name": "Opera",
+          "id": "Opera",
+          "data": [
+            [
+              "v50.0",
+              0.96
+            ],
+            [
+              "v49.0",
+              0.82
+            ],
+            [
+              "v12.1",
+              0.14
+            ]
+          ]
+        }
+      ]
+    }
+});
+//Chart Pantalones Slimns
+var chart4 = Highcharts.chart('chart2', {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Sales by month for: '
+    },
+    subtitle: {
+      text: 'Sales by Month'
+    },
+    xAxis: {
+      type: 'category'
+    },
+    yAxis: {
+      title: {
+        text: 'Ventas'
+      }
+  
+    },
+    legend: {
+      enabled: false
+    },
+    plotOptions: {
+      series: {
+        borderWidth: 0,
+        dataLabels: {
+          enabled: true,
+          format: '{point.y:.1f}'
+        }
+      }
+    },
+  
+    tooltip: {
+      headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+      pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b> total of sales.<br/>'
+    },
+  
+    "series": [
+      {
+        "name": "Months",
+        "colorByPoint": true,
+        "data": [
+          {
+            "name": "January",
+            "y": 600,
+            "drilldown": "Chrome"
+          },
+          {
+            "name": "February",
+            "y": 400,
+            "drilldown": "Firefox"
+          },
+          {
+            "name": "March",
+            "y": 250,
+            "drilldown": "Internet Explorer"
+          },
+          {
+            "name": "April",
+            "y": 400,
+            "drilldown": "Safari"
+          }
+        ]
+      }
+    ],
+    "drilldown": {
+      "series": [
+        {
+          "name": "Chrome",
+          "id": "Chrome",
+          "data": [
+            [
+              "v65.0",
+              0.1
+            ],
+            [
+              "v64.0",
+              1.3
+            ],
+            [
+              "v63.0",
+              53.02
+            ],
+            [
+              "v62.0",
+              1.4
+            ],
+            [
+              "v61.0",
+              0.88
+            ],
+            [
+              "v60.0",
+              0.56
+            ],
+            [
+              "v59.0",
+              0.45
+            ],
+            [
+              "v58.0",
+              0.49
+            ],
+            [
+              "v57.0",
+              0.32
+            ],
+            [
+              "v56.0",
+              0.29
+            ],
+            [
+              "v55.0",
+              0.79
+            ],
+            [
+              "v54.0",
+              0.18
+            ],
+            [
+              "v51.0",
+              0.13
+            ],
+            [
+              "v49.0",
+              2.16
+            ],
+            [
+              "v48.0",
+              0.13
+            ],
+            [
+              "v47.0",
+              0.11
+            ],
+            [
+              "v43.0",
+              0.17
+            ],
+            [
+              "v29.0",
+              0.26
+            ]
+          ]
+        },
+        {
+          "name": "Firefox",
+          "id": "Firefox",
+          "data": [
+            [
+              "v58.0",
+              1.02
+            ],
+            [
+              "v57.0",
+              7.36
+            ],
+            [
+              "v56.0",
+              0.35
+            ],
+            [
+              "v55.0",
+              0.11
+            ],
+            [
+              "v54.0",
+              0.1
+            ],
+            [
+              "v52.0",
+              0.95
+            ],
+            [
+              "v51.0",
+              0.15
+            ],
+            [
+              "v50.0",
+              0.1
+            ],
+            [
+              "v48.0",
+              0.31
+            ],
+            [
+              "v47.0",
+              0.12
+            ]
+          ]
+        },
+        {
+          "name": "Internet Explorer",
+          "id": "Internet Explorer",
+          "data": [
+            [
+              "v11.0",
+              6.2
+            ],
+            [
+              "v10.0",
+              0.29
+            ],
+            [
+              "v9.0",
+              0.27
+            ],
+            [
+              "v8.0",
+              0.47
+            ]
+          ]
+        },
+        {
+          "name": "Safari",
+          "id": "Safari",
+          "data": [
+            [
+              "v11.0",
+              3.39
+            ],
+            [
+              "v10.1",
+              0.96
+            ],
+            [
+              "v10.0",
+              0.36
+            ],
+            [
+              "v9.1",
+              0.54
+            ],
+            [
+              "v9.0",
+              0.13
+            ],
+            [
+              "v5.1",
+              0.2
+            ]
+          ]
+        },
+        {
+          "name": "Edge",
+          "id": "Edge",
+          "data": [
+            [
+              "v16",
+              2.6
+            ],
+            [
+              "v15",
+              0.92
+            ],
+            [
+              "v14",
+              0.4
+            ],
+            [
+              "v13",
+              0.1
+            ]
+          ]
+        },
+        {
+          "name": "Opera",
+          "id": "Opera",
+          "data": [
+            [
+              "v50.0",
+              0.96
+            ],
+            [
+              "v49.0",
+              0.82
+            ],
+            [
+              "v12.1",
+              0.14
+            ]
+          ]
+        }
+      ]
+    }
+});
+
 // Modify type of chart
   $('#plain').click(function () {
     chart.update({
@@ -586,15 +1162,6 @@ var chart2 = Highcharts.chart('chart2', {
       }
     });
 
-    chart2.update({
-        chart: {
-          inverted: false,
-          polar: false
-        },
-        subtitle: {
-          text: 'Plain'
-        }
-      });
 
   });
   $('#plain2').click(function () {
@@ -620,15 +1187,7 @@ var chart2 = Highcharts.chart('chart2', {
         text: 'Inverted'
       }
     });
-    chart2.update({
-        chart: {
-          inverted: true,
-          polar: false
-        },
-        subtitle: {
-          text: 'Inverted'
-        }
-      });
+    
   });
   $('#inverted2').click(function () {
     chart2.update({
@@ -652,15 +1211,6 @@ var chart2 = Highcharts.chart('chart2', {
         text: 'Polar'
       }
     });
-    chart2.update({
-        chart: {
-          inverted: false,
-          polar: true
-        },
-        subtitle: {
-          text: 'Polar'
-        }
-      });
   });
   $('#polar2').click(function () {
     chart2.update({
--- public/js/conf.js
+++ public/js/conf.js
@@ -7,6 +7,13 @@ $(function() {
     });
 });
 
+$(function() {
+    $('#producto').change(function(){
+        $('.producto').hide();
+        $('#' + $(this).val()).show();
+    });
+});
+
 //Definicion de las variables correspondientes para los combobox
 var opt_pantalones = new Array ("-", "Skinny", "Slim");
 var opt_camisas = new Array ("-", "Polo", "Casual");
@@ -22,6 +29,34 @@ function cambia(){
     if(cosa!=0){
         //selecionamos las cosas Correctas
         mis_opts=eval("opt_" + cosa);
+        //se calcula el numero de cosas
+        num_opts=mis_opts.length;
+        //marco el numero de opt en el select
+        document.formulario1.opt.length = num_opts;
+        //para cada opt del array, la pongo en el select
+        for(i=0; i<num_opts; i++){
+            document.formulario1.opt.options[i].value=mis_opts[i];
+            document.formulario1.opt.options[i].text=mis_opts[i];
+        }
+        }else{
+            //si no habia ninguna opt seleccionada, elimino las cosas del select
+            document.formulario1.opt.length = 1;
+            //ponemos un guion en la unica opt que he dejado
+            document.formulario1.opt.options[0].value="-";
+            document.formulario1.opt.options[0].text="-";
+        }
+        //hacer un reset de las opts
+        document.formulario1.opt.options[0].selected = true;  
+    }
+
+function cambia1(){
+    var opt;
+    //Se toma el valor de la "opt seleccionada"
+    opt = document.formulario1.opt[document.formulario1.opt.selectedIndex].value;
+    //se chequea si la "cosa" esta definida
+    if(opt!=0){
+        //selecionamos las cosas Correctas
+        mis_opts=eval("opt_" + opt);
         //se calcula el numero de cosas
         num_opts=mis_opts.length;
         //marco el numero de opt en el select
