c63957e8e16a8202fb518e037894e0c3fa04b6ba
MarcoPC <marcomatute@unah.hn>
2019-01-11 02:36:17 -0600

    Estructura de comentarios

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+//Servidor estatico para las vistas HTML que se utilizaron en este ejercicio
 app.use(express.static("public"));
 
 //Levantar el servidor y escuchar en el puerto indicado
